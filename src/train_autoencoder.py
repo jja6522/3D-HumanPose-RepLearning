5a3baf2d3f58c29c351d4cdcfef79dee7f355dc4
Johann Alvarado <jja6522@rit.edu>
2023-04-22 23:06:59 -0400

    Added motion encoder/decoder GRU + MLP layers

--- src/train_autoencoder.py
+++ src/train_autoencoder.py
@@ -25,14 +25,18 @@ for gpu in gpus:
 ###########################################################
 RANDOM_SEED = 42
 
+# Hyperparameters from the paper DLow
 batch_size = 64
-num_vae_epoch = 10
-num_vae_data_sample = 5000
-vae_lr = 1.e-3
+num_epochs = 50
+samples_per_epoch = 5000
+learning_rate = 1.e-3 # Adam
+
 all_algos = ['ae']
-nk = 5
-t_his = 25
-t_pred = 100
+traj_dim = 48 # Stacked number of joints for training
+t_his = 25 # number of past motions (c)
+t_pred = 100 # number of future motions (t)
+
+nk = 5 # sample images for reconstruction
 
 
 def set_seed(seed):
@@ -48,15 +52,21 @@ class AE(keras.Model):
     def __init__(self, name='Autoencoder'):
         super(AE, self).__init__(name=name)
 
+        # Encoder architecture
         self.encoder = models.Sequential([
-            layers.InputLayer(input_shape=(125, 48, )),
-            layers.GRU(units=128, return_sequences=True)
+            layers.InputLayer(input_shape=(t_his ,traj_dim)),
+            layers.GRU(units=128, return_sequences=False),
+            layers.RepeatVector(t_pred),
+            layers.Dense(300, activation='tanh', name='enc_mlp1'),
+            layers.Dense(200, activation='tanh', name='enc_mlp2'),
         ], name="encoder")
 
+        # Decoder architecture
         self.decoder = models.Sequential([
-            layers.InputLayer(input_shape=(125, 128, )),
+            layers.InputLayer(input_shape=(t_pred, 200, )),
+            layers.Dense(300, activation='tanh', name='dec_mlp1'),
             layers.GRU(units=128, return_sequences=True),
-            layers.Dense(48)
+            layers.Dense(traj_dim, activation='tanh', name='dec_mlp2'),
         ], name="decoder")
 
     def encode(self, x):
@@ -92,21 +102,31 @@ if __name__ == "__main__":
 
         # Loss function, metrics and optimizer
         loss = tf.keras.losses.MeanSquaredError()
-        optimizer = tf.keras.optimizers.Adam(learning_rate=vae_lr)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
         training_loss_tracker = tf.keras.metrics.Mean(name="training_loss")
 
         train_stats = trange(0, desc='training_loss')
-        for r in tqdm(range(0, num_vae_epoch)):
-            generator = train_ds.sampling_generator(num_samples=num_vae_data_sample, batch_size=batch_size)
-            for x in generator:
-                #print(traj_np.shape)
-                x = x[..., 1:, :].reshape(x.shape[0], x.shape[1], -1)
-                #print(traj_np.shape)
+        for r in tqdm(range(0, num_epochs)):
+            generator = train_ds.sampling_generator(num_samples=samples_per_epoch, batch_size=batch_size)
+            for traj_np in generator:
+
+                # Remove the center hip joint for training
+                traj_np = traj_np[..., 1:, :]
+
+                # Stack all joints
+                traj_np = traj_np.reshape(traj_np.shape[0], traj_np.shape[1], -1)
+
+                # Transpose for selecting frames instead of batches
+                traj = np.ascontiguousarray(np.transpose(traj_np, (1, 0, 2)))
+
+                # Transpose back to batches and take past and future motions for encoding
+                x = np.transpose(traj[:t_his], (1, 0, 2))
+                y = np.transpose(traj[t_his:], (1, 0, 2))
 
                 with tf.GradientTape() as tape:
-                    z = autoencoder.encode(x)
-                    y_rec = autoencoder.decode(z)
-                    total_loss = loss(x, y_rec)
+                    x_enc = autoencoder.encode(x)
+                    y_rec = autoencoder.decode(x_enc)
+                    total_loss = loss(y, y_rec)
 
                 gradients = tape.gradient(total_loss, autoencoder.trainable_variables)
                 optimizer.apply_gradients(zip(gradients, autoencoder.trainable_variables))
@@ -114,8 +134,8 @@ if __name__ == "__main__":
                 train_stats.set_description('training_loss=%g' % training_loss_tracker.result())
 
         # Save the model to disk
-        autoencoder.encoder.save(f"models/encoder-{num_vae_epoch}.model")
-        autoencoder.decoder.save(f"models/decoder-{num_vae_epoch}.model")
+        autoencoder.encoder.save(f"models/encoder-{num_epochs}.model")
+        autoencoder.decoder.save(f"models/decoder-{num_epochs}.model")
 
     #######################################
     # Model Evaluation
@@ -123,22 +143,33 @@ if __name__ == "__main__":
     elif args.mode == 'test':
 
         autoencoder = AE(name='autoencoder')
-        autoencoder.encoder = models.load_model(f"models/encoder-{num_vae_epoch}.model")
-        autoencoder.decoder = models.load_model(f"models/decoder-{num_vae_epoch}.model")
+        autoencoder.encoder = models.load_model(f"models/encoder-{num_epochs}.model")
+        autoencoder.decoder = models.load_model(f"models/decoder-{num_epochs}.model")
 
         # Loss function and metrics
         loss = tf.keras.losses.MeanSquaredError()
         testing_loss_tracker = tf.keras.metrics.Mean(name="testing_loss")
 
         test_stats = trange(0, desc='testing_loss')
-        test_generator = test_ds.sampling_generator(num_samples=num_vae_data_sample, batch_size=batch_size)
-        for x in test_generator:
-            x = x[..., 1:, :].reshape(x.shape[0], x.shape[1], -1)
+        test_generator = test_ds.sampling_generator(num_samples=samples_per_epoch, batch_size=batch_size)
+        for traj_np in test_generator:
 
-            # Evaluat without updating gradients
-            z = autoencoder.encode(x)
-            y_rec = autoencoder.decode(z)
-            total_loss = loss(x, y_rec)
+            # Remove the center hip joint for training
+            traj_np = traj_np[..., 1:, :]
+
+            # Stack all joints
+            traj_np = traj_np.reshape(traj_np.shape[0], traj_np.shape[1], -1)
+
+            # Transpose for selecting frames instead of batches
+            traj = np.ascontiguousarray(np.transpose(traj_np, (1, 0, 2)))
+
+            # Transpose back to batches and take past and future motions for encoding
+            x = np.transpose(traj[:t_his], (1, 0, 2))
+            y = np.transpose(traj[t_his:], (1, 0, 2))
+
+            x_enc = autoencoder.encode(x)
+            y_rec = autoencoder.decode(x_enc)
+            total_loss = loss(y, y_rec)
 
             testing_loss_tracker.update_state(total_loss)
             test_stats.set_description('testing_loss=%g' % testing_loss_tracker.result())
@@ -149,8 +180,8 @@ if __name__ == "__main__":
     elif args.mode == 'inference':
 
         autoencoder = AE(name='autoencoder')
-        autoencoder.encoder = models.load_model(f"models/encoder-{num_vae_epoch}.model")
-        autoencoder.decoder = models.load_model(f"models/decoder-{num_vae_epoch}.model")
+        autoencoder.encoder = models.load_model(f"models/encoder-{num_epochs}.model")
+        autoencoder.decoder = models.load_model(f"models/decoder-{num_epochs}.model")
 
         #######################################
         # Inference
@@ -160,62 +191,72 @@ if __name__ == "__main__":
             algos.append(algo)
         vis_algos = algos.copy()
 
-        def get_prediction(data, algo, sample_num, num_seeds=1, concat_hist=True):
+        def get_prediction(traj_np, algo, sample_num, num_seeds=1, concat_hist=True):
 
-            # Flatten the sample pose for passing through the network
-            x = data[..., 1:, :].reshape(data.shape[0], data.shape[1], -1)
-            print(x.shape)
+            # Remove the center hip joint for training
+            traj_np = traj_np[..., 1:, :]
 
-            # Take only the last t_hist frames
-            x = x[:t_his]
-            print(x.shape)
+            # Stack all joints
+            traj_np = traj_np.reshape(traj_np.shape[0], traj_np.shape[1], -1)
+
+            # Transpose for selecting frames instead of batches
+            traj = np.ascontiguousarray(np.transpose(traj_np, (1, 0, 2)))
+
+            # Transpose back to batches and take past and future motions for encoding
+            x = np.transpose(traj[:t_his], (1, 0, 2))
+            y = np.transpose(traj[t_his:], (1, 0, 2))
 
             # Generate multiple samples
             x_multi = tf.repeat(x, repeats = [sample_num * num_seeds], axis=0)
-            print(x_multi.shape)
 
             # Apply the autoencoder
             z = autoencoder.encode(x_multi)
             y_rec_multi = autoencoder.decode(z)
             y_rec_multi = y_rec_multi.numpy()
-            print(y_rec_multi.shape)
 
-            if y_rec_multi.shape[0] > 1:
-                y_rec_multi = y_rec_multi.reshape(-1, sample_num, y_rec_multi.shape[-2], y_rec_multi.shape[-1])
+            # Merge the initial and predicted frames
+            merged_frames = np.concatenate((x_multi, y_rec_multi), axis=1)
+
+            if merged_frames.shape[0] > 1:
+                merged_frames = merged_frames.reshape(-1, sample_num, merged_frames.shape[-2], merged_frames.shape[-1])
             else:
-                y_rec_multi = y_rec_multi[None, ...]
-            return y_rec_multi
+                merged_frames = merged_frames[None, ...]
+
+            return merged_frames
 
         def post_process(pred, data):
-            # Unflatten reconstructed poses for evaluation
-            pred = pred.reshape(pred.shape[0], pred.shape[1], -1, 3)
-            pred = np.concatenate((np.tile(data[..., :1, :], (pred.shape[0], 1, 1, 1)), pred), axis=2)
+
+            # Reshape the stacked poses into individual joints
+            pred_joints = pred.reshape(pred.shape[0], pred.shape[1], -1, 3)
+
+            # Extract the center hip joint from the original data (This joint was removed for training)
+            center_joint = np.tile(data[..., :1, :], (pred_joints.shape[0], 1, 1, 1))
+
+            # Merge the center joint and predicted ones
+            pred = np.concatenate((center_joint, pred_joints), axis=2)
+
+            # Set the center joint to zero to center the image?
             pred[..., :1, :] = 0
             return pred
 
         def pose_generator():
 
             while True:
-                # Take a test sample and test its reconstructed pose
-                sample_pose = test_ds.sample()
-                print("Sampled Pose:", sample_pose.shape)
+                data = test_ds.sample()
 
                 # gt
-                gt = sample_pose[0].copy()
+                gt = data[0].copy()
                 gt[:, :1, :] = 0
                 poses = {'context': gt, 'gt': gt}
 
                 # ae
                 for algo in vis_algos:
-                    pred = get_prediction(sample_pose, algo, nk)[0]
-                    print(pred.shape)
-                    pred = post_process(pred, sample_pose)
-                    print(pred.shape)
+                    pred = get_prediction(data, algo, nk)[0]
+                    pred = post_process(pred, data)
                     for i in range(1, pred.shape[0] + 1):
                         poses[f'{algo}_{i}'] = pred[i-1]
                 yield poses
 
         pose_gen = pose_generator()
-
         render_animation(test_ds.skeleton, pose_gen, vis_algos, t_his, fix_0=True, output=None, size=5, ncol=7, interval=50)
 
